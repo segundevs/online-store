fc7d6a7beff51472ef8de80a493a20e41cbd35e4
segundevs <tushbash@gmail.com>
2021-06-22 12:58:57 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import { fetchData } from './components/FetchData';
+import { useQuery } from 'react-query';
+
 
 function App() {
+
+  const {data, isLoading, isError} = useQuery('products', () => fetchData('https://fakestoreapi.com/products'), {
+      retry: false
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {isLoading && <h3>Data is loading...</h3>}
+      {isError && <h4>There was an error with that resource</h4>}
+      {data && console.log(data)}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+
+const Header = () => {
+  return (
+    <Box  as="button" borderRadius='md' m={6} bg="#171923" px={4} color="#fff" h={8}>
+      This is the header
+    </Box>
+  )
+}
+
+export default Header
