3118403dde47f9296e0be88c2bbd5a961702eabe
segundevs <tushbash@gmail.com>
2021-07-22 13:48:03 -0700

    minor change

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -10,6 +10,7 @@ const Shop = () => {
     <>
       <h1 className="text-gray-700 dark:text-gray-200 text-center text-4xl my-6 font-black">All Products</h1>
       <div className="lg:grid lg:grid-cols-3 lg:gap-4">
+        {isError && console.log(isError.message)}
         {data && data.map((item)=>(
           <Card item={item} key={item.id}/>
         ))}
